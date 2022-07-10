a11e806daa42c1a3f439e4be952a44b1d8b867c3
New Slave <banana13373228@gmail.com>
2022-07-10 21:21:02 +0300

    first commit

--- components/Navbar-Bt.vue
+++ components/Navbar-Bt.vue
@@ -2,14 +2,35 @@
     <div>
         <nav class="navbar navbar-dark bg-dark navbar-expand-lg">
   <div class="container">
-     <a class="navbar-brand" href="">Ui component</a>
+      <a href='/' class="navbar-brand" href="">Ui component</a> 
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarScroll">
       <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
+        <li class="nav-item">
+          <a href='/' class="nav-link mt-1" aria-current="page"> Home</a> 
+        </li>
+        <li class="nav-item">
+          <a href='/button' class="nav-link mt-1" aria-current="page"> Button</a> 
+        </li>
+        <li class="nav-item">
+         <a  href='/checkbox' class="nav-link mt-1" aria-current="page"> Checkboxs</a>
+        </li>
+        <li class="nav-item">
+         <a href='/select'  class="nav-link mt-1" aria-current="page"> Select</a> 
+        </li>
+        <li class="nav-item">
+         <a href='/navbar'   class="nav-link mt-1" aria-current="page"> Navbar</a> 
+        </li>
+        <li class="nav-item">
+        <a href='/toggle'  class="nav-link mt-1" aria-current="page"> Toggle Navbar</a> 
+        </li>
+        <li class="nav-item">
+          <a href='/vibration' class="nav-link mt-1" aria-current="page"> Vibration</a>
+        </li>
       </ul>
-
+      
       <!-- <form class="d-flex" role="search">
         <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
         <button class="btn btn-outline-success" type="submit">Search</button>
@@ -33,5 +54,7 @@ export default {
         color: rgb(163, 161, 161);
         text-decoration: none;
     }
-
+    .nuxt-link-exact-active a{
+      color: rgb(91, 167, 221);
+}
 </style>
--- dist/_nuxt/static/1657476905/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002Fvibration","\u002F","\u002Fbutton","\u002Fselect","\u002Fnavbar","\u002Ftoggle","\u002Fcheckbox"]})
