f912c705c67e8c7bec68f89289f2164069ff246d
New Slave <banana13373228@gmail.com>
2022-07-10 20:29:37 +0300

    first commit

--- nuxt.config.js
+++ nuxt.config.js
@@ -66,4 +66,10 @@ export default {
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
   },
+
+  target: "static",
+  ssr: 'false',
+  generate: {
+       fallback: true
+  },
 }
